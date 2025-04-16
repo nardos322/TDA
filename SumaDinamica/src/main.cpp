48db9dfd1092fcbc248aaed66a1b6f6a37b81af0
nardos322 <nahuel.rlz@gmail.com>
2025-04-16 16:35:54 -0300

    fix sumaDinamica

--- SumaDinamica/src/main.cpp
+++ SumaDinamica/src/main.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 using namespace std;
 
-void backtrack(const int k, int i, const vector<int>& set, vector<int>& permutacion, vector<bool>& visitado) {
-    if (i == set.size()) {
-        for (int j = 0; j < permutacion.size(); j++) {
-            cout << permutacion[j] << " ";
+void backtrack(const int k, int i, int acumulador, const vector<int>& set, vector<int>& permutacion, vector<bool>& visitado) {
+
+    if (acumulador == k) {
+        for (const int n : permutacion) {
+            cout << n << " ";
         }
         cout << endl;
         return;
@@ -16,20 +17,28 @@ void backtrack(const int k, int i, const vector<int>& set, vector<int>& permutac
         if (!visitado[j]) {
             permutacion[i] = set[j];
             visitado[j] = true;
-            backtrack(k, i + 1, set, permutacion, visitado);
+            backtrack(k, i + 1, acumulador + set[j], set, permutacion, visitado);
             visitado[j] = false;
+            permutacion[i] = 0;
         }
 
     }
 
 }
 
-
-int main() {
-    const vector<int> set = {6, 12, 6};
+bool existe_suma_subconjunto(const vector<int>& set) {
     vector<int> permutacion(set.size());
     vector<bool> visitado(set.size(), false);
-    backtrack(6, 0, set, permutacion, visitado);
 
+
+
+}
+
+
+int main() {
+    const vector<int> set = {1, 1, 3};
+    vector<int> permutacion(set.size());
+    vector<bool> visitado(set.size(), false);
+    backtrack(5, 0, 0, set, permutacion, visitado);
     return 0;
 }
