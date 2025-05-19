42f0ae5938cde514eff012e2c1026d1b467db855
nardos322 <nahuel.rlz@gmail.com>
2025-05-19 00:09:16 +0000

    fix algoritmo greedy

--- greedy/sumaGolosa/src/main.cpp
+++ greedy/sumaGolosa/src/main.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 
-int suma_golosa(const vector<int>& v, const int i, const int j, int costo_total) {
-
-    if ( j >= v.size()) {
+// implementacion incorrecta, no funciona para todos los casos
+int suma_golosa(vector<int>& v, const int i, int costo_total) {
+    if ( i >= v.size()) {
         return 0;
     }
 
     if (costo_total == 0) {
-        costo_total = v[i] + v[j];
+        costo_total = v[i - 1] + v[i];
     } else {
-        costo_total += v[j];
+        costo_total += v[i];
     }
 
-    return costo_total + suma_golosa(v, i + 1, j + 1, costo_total);
+    return costo_total + suma_golosa(v, i + 1, costo_total);
 
 }
 
 
+// implementacion corecta
+int suma_golosa_2(vector<int>& v) {
+    priority_queue<int, vector<int>, greater<int>> heap(v.begin(), v.end());
+    int costo_total = 0;
+
+    while (heap.size() > 1) {
+        const int a = heap.top(); heap.pop();
+        const int b = heap.top(); heap.pop();
+
+        int suma = a + b;
+        costo_total += suma;
+        heap.push(suma);
+    }
+
+    return costo_total;
+}
+
 int main() {
 
-    const vector<int> v = {1, 2, 5};
-    const int res = suma_golosa(v, 0, 1, 0);
-
+    vector v = {1, 2, 3, 4, 5};
+    const int res = suma_golosa(v, 1, 0);
     cout << "El resultado es: " << res << endl;
+    const int res2 = suma_golosa_2(v);
+    cout <<"El resultado es: " << res2 << endl;
 
     return 0;
 }
