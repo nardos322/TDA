38bb750f7ea3fc742f41fab6c5786800fc16d925
nardos322 <nahuel.rlz@gmail.com>
2025-09-10 20:35:18 -0300

    fix taller 1 c

--- tp/ale_1s/main.cpp
+++ tp/ale_1s/main.cpp
@@ -1,42 +1,72 @@
 #include <iostream>
-#include <vector>
 
 
-std::vector<int> combinar(std::vector<int> a, std::vector<int> b, std::vector<int> c) {
+
+long long ale_1s(const long long n, const long long l, const long long r, long long& indice) {
+    long long contador_1s = 0;
+
+    if (n > 1) contador_1s += ale_1s(n / 2, l, r, indice);
+
+    indice++;
+
+    if (indice >= l && indice <= r && (n % 2) == 1) contador_1s++;
+
+    //std::cout << n << ":"<< indice << ", "<< resto  << " " << "["<<l << ":" << r << "]"<< std::endl;
+    if (n > 1) contador_1s += ale_1s(n / 2, l, r, indice);
 
 
-    std::vector<int> res;
-    res.reserve(a.size() + b.size() + c.size());
-    res.insert(res.end(), a.begin(), a.end());
-    res.insert(res.end(), b.begin(), b.end());
-    res.insert(res.end(), c.begin(), c.end());
-    return res;
+    return contador_1s;
+
+
 }
 
-std::vector<int> ale_1s(std::vector<int>&v, const int n) {
-    std::vector<int> res;
 
-    if (n > 1) {
-        const std::vector<int> a = ale_1s(v, n / 2);
-        const std::vector<int> b = ale_1s(v, n % 2);
-        const std::vector<int> c = ale_1s(v, n / 2);
-        res = combinar(a , b , c);
-        return res;
+void solve(std::istream& in, std:: ostream& out) {
+    long long n, l, r;
+    while (in >> n >> l >> r) {
+        long long contador = 0;
+        const long long res = ale_1s(n, l, r, contador);
+        out << res << '\n';
     }
-    res.push_back(n);
-    return res;
 }
 
 
-
-
+#define LOCAL
+#ifndef LOCAL
 
 int main() {
-    std::vector<int> v;
 
-    for (const std::vector<int> res = ale_1s(v,6); const auto i : res) {
-        std::cout << i ;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve(std::cin, std::cout);
 
-    }
     return 0;
 }
+#else
+#include <sstream>
+#include <cassert>
+
+static std::string run_solve(const std::string& input) {
+    std::istringstream in (input);
+    std::ostringstream out;
+    solve(in, out);
+    return out.str();
+}
+
+int main() {
+
+    assert(run_solve("6 1 7\n") == "6\n");  // inorder 6: [1,3,1,6,1,3,1]
+    assert(run_solve("6 3 6\n") == "3\n");  // [1,6,1,3] => 3 impares
+    assert(run_solve("4 1 7\n") == "4\n");  // [1,2,1,4,1,2,1]
+    assert(run_solve("3 1 3\n") == "3\n");  // [1,3,1]
+    assert(run_solve("3 10 20\n") == "0\n");// rango fuera -> 0
+    assert(run_solve("9 4 11\n") == "5\n");
+
+    std::cerr << "Tests LOCAL OK\n";
+    return 0;
+
+}
+
+
+
+#endif
