30ec9ac6ecb54da46dfed73f9f70e7c9fe5ad36e
nardos322 <nahuel.rlz@gmail.com>
2025-09-24 14:06:52 -0300

    init taller 2 b

---
+++ tp/borrar/borrar.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void f(int l, int r) {
+
+}
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+   cout << "Hello, World!" << endl;
+
+    return 0;
+}
